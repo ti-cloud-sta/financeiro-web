e0e5808806b95b6a6bcbf08f3d87cd59adb7ec42
Joeder Blanca <joebblanca@gmail.com>
2026-09-20 15:51:06 -0300

    limpeza

--- alter_historico.sql
+++
@@ -1,5 +0,0 @@
--- Script para adicionar rastreamento de fase e status no Histórico de Pendências
--- Execute este script no seu banco de dados MySQL
-
-ALTER TABLE historicopendencia ADD COLUMN fase VARCHAR(50) DEFAULT NULL;
-ALTER TABLE historicopendencia ADD COLUMN status VARCHAR(50) DEFAULT NULL;
